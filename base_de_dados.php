440763902e9af07eb68785fa5aecbf8889099b6d
Gusttavo210 <102116359+Gusttavo210@users.noreply.github.com>
2022-03-23 00:32:35 -0300

    Create base_de_dados.php

---
+++ base_de_dados.php
@@ -0,0 +1,48 @@
++<?php
+# https://www.php.net/manual/pt_BR/reserved.classes.php
+$Home = array(
+    (object) [
+        'imagem' => 'home-bg.jpg',
+        'titulo' => 'Clean Blog',
+        'subheading' => 'A Blog Theme by Start Bootstrap'
+    ],
+    
+);
+
+
+);
+
+$about = array(
+    (object)[
+        'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?'
+    ],
+    (object)[
+        'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!'
+    ],
+    (object)[
+        'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!'
+    ],
+   
+);
+
+$post = array(
+    (object)[
+        'post-title' => 'Man must explore, and this is exploration at its greatest',
+        'post-subtitle'=>'Problems look mighty small from 150 miles up',
+        'data' => 'Posted by Start Bootstrap on September 24, 2021'
+    ],
+    (object)[
+        'post-title' => "I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.",
+        'data' => 'Posted by Start Bootstrap on September 18, 2021'
+    ],
+    (object)[
+        'post-title' => 'Science has not yet mastered prophecy',
+        'post-subtitle'=>'We predict too much for the next year and yet far too little for the next ten.',
+        'data' => 'Posted by Start Bootstrap  on August 24, 2021'
+    ]
+    (object)[
+        'post-title' => 'Failure is not an option',
+        'post-subtitle'=>'Many say exploration is part of our destiny, but it’s actually our duty to future generations.',
+        'data' => 'Posted by Start Bootstrap on July 8, 2021'
+    ]
+);
